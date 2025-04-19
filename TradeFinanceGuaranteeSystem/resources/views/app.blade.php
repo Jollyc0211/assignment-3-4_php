76bebfa5ca76f69a3ffcb99c9ab62a28062328ca
vinamra842000 <vinamra842000@gmail.com>
2025-04-19 02:06:01 -0400

    Remove old to replace with new

--- TradeFinanceGuaranteeSystem/resources/views/app.blade.php
+++
@@ -1,6 +0,0 @@
-@if (Auth::check())
-    <a href="{{ route('dashboard') }}">Dashboard</a>
-    <a href="{{ route('logout') }}">Logout</a>
-@else
-    <a href="{{ route('login') }}">Login</a>
-@endif
--- TradeFinanceGuaranteeSystem/resources/views/upload.blade.php
+++
@@ -1,5 +0,0 @@
-<form action="{{ route('files.upload') }}" method="POST" enctype="multipart/form-data">
-    @csrf
-    <input type="file" name="file">
-    <button type="submit">Upload</button>
-</form>
